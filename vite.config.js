fff0fd18f4d0deffff11f4b7fc30279a07f1cc34
Abhishek Rajput <abhishekrajput3172@gmail.com>
2024-06-19 14:16:44 +0530

    add build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'public',
   },
-  base: '/React/' // This should match your repository name
+  // base: '/React/' / This should match your repository name
 });
